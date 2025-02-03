32f43b61e9481fd90ca7b30c013d9416af811b11
89Explorer <leral3@naver.com>
2025-02-04 07:34:50 +0900

    Feat: 투데이 화면에 테이블뷰 생성

--- MovieClip/ViewController/Core/HomeViewController.swift
+++ MovieClip/ViewController/Core/HomeViewController.swift
@@ -9,21 +9,59 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    
+    // MARK: - UI Component
+    private let homeFeedTableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        return tableView
+    }()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        configureConstraints()
+        setupTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Functions
+    /// 테이블뷰 델리게이트 설정
+    private func setupTableView() {
+        homeFeedTableView.delegate = self
+        homeFeedTableView.dataSource = self
+        homeFeedTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-    */
+    
+    // MARK: - Layouts
+    /// 제약조건
+    private func configureConstraints() {
+        view.addSubview(homeFeedTableView)
+        
+        homeFeedTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            homeFeedTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            homeFeedTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            homeFeedTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            homeFeedTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+            
+        ])
+    }
+}
 
+// MARK: - Extension: TableView Delegate
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell 
+    }
 }
