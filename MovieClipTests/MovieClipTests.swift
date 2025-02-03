1525b61bf41f3f4adc9891de00a03c038d0cf8ae
89Explorer <leral3@naver.com>
2025-02-04 04:41:03 +0900

    Initial Commit

---
+++ MovieClipTests/MovieClipTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieClipTests.swift
+//  MovieClipTests
+//
+//  Created by 권정근 on 2/4/25.
+//
+
+import Testing
+@testable import MovieClip
+
+struct MovieClipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
